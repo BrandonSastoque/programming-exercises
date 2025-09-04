33260208dde6e38a0f5d856752c65d7a93cee131
BrandonSastoque <sasvilldesings@gmail.com>
2025-09-04 13:59:22 -0500

    Primer Commit: Hola Mundo

---
+++ ejercicio1.py
@@ -0,0 +1,29 @@
+# URL PYTHON: https://www.python.org/
+
+# Comentario de una línea 
+
+"""
+Comentario 
+de 
+varias 
+líneas
+"""
+
+# Variable
+variable_1 = 1
+
+# Constante (Python no tiene constantes reales, pero por convención se escriben en mayúsculas)
+CONSTANTE = 3.14
+
+# Tipos de datos primitivos
+entero = 3
+flotante = 2.1
+booleano_1 = True
+booleano_2 = False
+cadena = "Hola a todos"
+
+# Imprimir mensaje
+print("¡Hola, Python!")
+
+
+#Finalizado dia 1, correcciones en el cuaderno!
