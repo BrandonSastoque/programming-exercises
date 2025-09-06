1ded2267d8f74a796244dd7f381fd94c921ac6e7
BrandonSastoque <sasvilldesings@gmail.com>
2025-09-06 12:54:54 -0500

    Calculadora basica

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,4 +1,7 @@
 numero1 = int(input("Digite el primero numero:\n"))
 numero2 = int(input("Digite el segundo numero:\n"))
 
-print("La suma de", numero1,"y", numero2, "es: ", numero1+numero2)
+print("La suma de", numero1,"y", numero2, "es:", numero1+numero2)
+print("La resta de", numero1,"y", numero2, "es:", numero1-numero2)
+print("La multiplicacion de", numero1,"y", numero2, "es:", numero1*numero2)
+print("La division de", numero1,"y", numero2, "es:", numero1/numero2)
