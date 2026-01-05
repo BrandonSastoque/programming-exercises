af7c0f63efa81eae72764ee0f86c1261070c0f12
BrandonSastoque <sasvilldesings@gmail.com>
2026-01-05 12:11:01 -0500

    Analizador Financiero 2

--- calculasueldo.py
+++ calculasueldo.py
@@ -10,5 +10,16 @@ ahorro_viaje = Salario_mensual * 0.20
 tiempo_imagen = Costo_imagen / ahorro_imagen
 tiempo_viaje = Costo_viaje / ahorro_viaje
 
+#Calculo meses
 print("Para renovar tu imagen necesitas",round(tiempo_imagen, 1),"meses")
 print("Para tu viaje necesitas", round(tiempo_viaje, 1),"meses")
+
+#Consejo
+if tiempo_viaje > 24:
+    print ("NECESITAS UN INGRESO EXTRA PARA LLEGAR A LA META")
+
+elif tiempo_viaje > 12 and  tiempo_viaje <=24:
+    print("VAS POR BUEN CAMINO SIGUE ASI")
+
+else:
+    print ("ESTAS MUY CERCA")
